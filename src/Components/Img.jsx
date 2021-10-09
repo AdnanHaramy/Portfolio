aff89f6bb907e48ea805723e3a62f7827554b1af
Adnan Haramy <82480594+AdnanHaramy@users.noreply.github.com>
2021-10-10 00:18:02 +0300

    Add files via upload

---
+++ src/Components/Img.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import '../Components/styles.css'
+import adnanHaramy from "../images/adnanHaramy.jpg"
+function Image () {
+return <div className="imgContainer">
+<img 
+className = "adnanImg"
+src={adnanHaramy} 
+alt="" />
+<p>
+I have more than two years work experience in front-end and have a firm knowledge of HTML, CSS and JavaScript 
+language and possess a good knowledge of computer software packages (frameworks and tools) that are used in today’s technology. <br />
+On a personal level, I am highly-motivated, result oriented, self-driven, hard-working, fast learner and constantly seeking to improve my skills and am fully aware of the latest Front-end Development Tools.
+In addition to this, I have the ability to adapt to any type of team environment, I am team oriented and get along with others when working in a group setting. I also have the ability to work independently while staying on schedule and meeting those tight deadlines.   
+</p>
+</div>;
+
+
+}
+export default Image;
