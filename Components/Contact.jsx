8f75f7be4498a2b68caab6f754ecbfd1fa768fce
Adnan Haramy <82480594+AdnanHaramy@users.noreply.github.com>
2023-09-25 13:20:12 +0300

    Update Contact.jsx

--- Components/Contact.jsx
+++ Components/Contact.jsx
@@ -15,3 +15,39 @@ function Contact() {
                 console.log(error.text);
             });
     }
+  return (
+        <section className="contact">
+            <div className="contact-container">
+                <div className="contact-left">
+                    <h2>Contact Me</h2>
+                    <form ref={form} onSubmit={sendEmail}>
+                        <div className="form-group" >
+                            <label>Name</label>
+                            <input type="text" name="user_name" placeholder="Your Name" />
+                        </div>
+                        <div className="form-group" >
+                            <label>Email</label>
+                            <input type="email" name="user_email" placeholder="Your Email" />
+                        </div>
+                        <div className="form-group" >
+                            <label>Message</label>
+                            <textarea name="message" />
+                            <input type="submit" name="user_email" placeholder="Your Message" />
+                        </div>
+                    </form>
+                </div>
+                <div className="contact-right">
+                    <h2>Get in Touch</h2>
+                    <div className="contact-logos">
+                        <Image src="/upwork.svg" alt="Logo 1" width={50} height={50} className="logo" />
+                        <Image src="/facebook.svg" alt="Logo 2" width={50} height={50} className="logo" />
+                        <Image src="/whatsapp.svg" alt="Logo 3" width={50} height={50} className="logo" />
+                        <Image src="/linkedin.svg" alt="Logo 4" width={50} height={50} className="logo" />
+                        <Image src="/twitter.svg" alt="Logo 5" width={50} height={50} className="logo" />
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
+}
+export default Contact;
