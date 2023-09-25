a6ebc9ab2d430cf4ac82ee66e8ee14c339a05d30
Adnan Haramy <82480594+AdnanHaramy@users.noreply.github.com>
2023-09-25 13:19:46 +0300

    Update Contact.jsx

--- Components/Contact.jsx
+++ Components/Contact.jsx
@@ -3,3 +3,15 @@ import React from 'react';
 import Image from 'next/image';
 import emailjs from '@emailjs/browser';
 import { useRef } from 'react';
+function Contact() {
+    const form = useRef();
+    const sendEmail = (e) => {
+        e.preventDefault();
+        emailjs.sendForm('key', 'key', form.current, 'key')
+            .then((result) => {
+                console.log(result.text);
+                e.target.reset();
+            }, (error) => {
+                console.log(error.text);
+            });
+    }
