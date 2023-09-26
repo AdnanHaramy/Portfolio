e4cae5b9b3f7a0692fd7a6168090b1a7f2171bab
Adnan Haramy <82480594+AdnanHaramy@users.noreply.github.com>
2023-09-26 12:03:25 +0300

    Update Testimonials.jsx

--- Components/Testimonials.jsx
+++ Components/Testimonials.jsx
@@ -1,1 +1,23 @@
+import React from 'react';
+import Slider from './Slider';
+function Testimonials() {
+    const images = ['/maher.png', '/maher.png', '/maher.png'];
+    const captions = ['Caption 1', 'Caption 2', 'Caption 3'];
+    return (
+        <section className="testimonials">
+            <div className="testimonials-container">
+                <div className="testimonials-left">
+                    <h2> ━━━ Testimonials</h2>
+                    <p>
+                        Happy Students
+                    </p>
+                </div>
+                <div className="testimonials-right">
+                    <Slider images={images} captions={captions} />
+                </div>
+            </div>
+        </section>
+    );
+}
+export default Testimonials;
 
