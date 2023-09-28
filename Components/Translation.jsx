80e1d02d4334bb1d1b7a7111148a2b73e8f42318
Adnan Haramy <82480594+AdnanHaramy@users.noreply.github.com>
2023-09-28 14:20:15 +0300

    Update Translation.jsx

--- Components/Translation.jsx
+++ Components/Translation.jsx
@@ -6,25 +6,27 @@ function Translation() {
             <div className="translation-container">
                 <div className="translation-left">
                     <h2>Translation </h2>
-                    <p>
-                        I have a wealth of experience in the field of literary translation, <br />
-                        including the translation of numerous books from Arabic to English <br />
-                        and vice versa. My dedication to the craft has resulted in the successful <br />
-                        publication of several acclaimed works, expanding the reach of these <br />
-                        literary treasures to a wider audience. My translations are known for <br />
-                        their ability to capture the essence and intricacies of literary works <br />
-                        while preserving the unique voice and narrative style of  the original  <br />
-                        authors. His portfolio encompasses a diverse range of genres, including <br />
-                        fiction, non-fiction, poetry, and academic texts, making each translation <br />
-                        a labor of passion and a bridge between cultures.
+                    <div className="translation-paragraph-container">
+                        <p>
+                            I have a wealth of experience in the field of literary translation,
+                            including the translation of numerous books from Arabic to English
+                            and vice versa. My dedication to the craft has resulted in the successful
+                            publication of several acclaimed works, expanding the reach of these
+                            literary treasures to a wider audience. My translations are known for
+                            their ability to capture the essence and intricacies of literary works
+                            while preserving the unique voice and narrative style of  the original
+                            authors. His portfolio encompasses a diverse range of genres, including
+                            fiction, non-fiction, poetry, and academic texts, making each translation
+                            a labor of passion and a bridge between cultures.
 
-                    </p>
+                        </p>
+                    </div>
                 </div>
                 <div className="translation-right">
                     <Slider
                         images={images}
-                        height={300}
-                        width={220}
+                        height={280}
+                        width={200}
                         borderRadius='2%' />
                 </div>
             </div>
